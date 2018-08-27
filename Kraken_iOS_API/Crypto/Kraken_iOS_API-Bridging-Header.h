21bda3d9a6c924a8f964c726e50c231507eac590
José Fonseca <josefonseca@ipg.pt>
2018-08-27 09:36:19 +0100

    New Bridging Header

---
+++ Kraken_iOS_API/Crypto/Kraken_iOS_API-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  BridgingHeader.h
+//  Trader
+//
+//  Created by José Fonseca on 26/08/2018.
+//  Copyright © 2018 Ze. All rights reserved.
+//
+
+
+#import <CommonCrypto/CommonCrypto.h>
+#import <CommonCrypto/CommonHMAC.h>
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#endif /* BridgingHeader_h */
